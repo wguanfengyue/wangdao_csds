4ebfeb418f7b9fd1fae039df998fe7d6a0ed5df9
wguanfengyue <2468287068@qq.com>
2025-12-06 12:32:09 +0800

    update code

--- Sept2/02ReverseList.c
+++ Sept2/02ReverseList.c
@@ -1,20 +1,40 @@
-#include<stdio.h>
-#include<stdlib.h>
+#include <stdio.h>
+#include <stdlib.h>
 #define MAXSIZE 100
 
-typedef struct {
+typedef struct
+{
     int data[MAXSIZE];
     int length;
-}SqList;
-void ReverseList(SqList *L){
-    for(int i=0;i<L->length/2;i++){
+} SqList;
+void ReverseList(SqList *L)
+{
+    for (int i = 0; i < L->length / 2; i++)
+    {
         int temp = 0;
         temp = L->data[i];
-        L->data[i] = L->data[L->length-1-i];
-        L->data[L->length-1-i] = temp;
+        L->data[i] = L->data[L->length - 1 - i];
+        L->data[L->length - 1 - i] = temp;
     }
 }
 
-int main(){
-    
-
+int main()
+{
+    SqList L;
+    L.data[0] = 1;
+    L.data[1] = 2;
+    L.data[2] = 3;
+    L.data[3] = 4;
+    L.length = 4;
+    printf("before\n");
+    for (int i = 0; i < L.length; i++)
+    {
+        printf("%d ", L.data[i]);
+    }
+    ReverseList(&L);
+    printf("\n");
+    for (int i = 0; i < L.length; i++)
+    {
+        printf("%d ", L.data[i]);
+    }
+}
