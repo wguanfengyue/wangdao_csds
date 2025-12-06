4f8e8a1de5a6d718e7f03694d5773924798a50e0
wguanfengyue <2468287068@qq.com>
2025-12-05 16:30:58 -0800

    cc

--- Sept2/01SqList.c
+++ Sept2/01SqList.c
@@ -3,58 +3,52 @@
 #define MAXSIZE 100
 
 typedef struct {
-    int length;
     int data[MAXSIZE];
+    int length;
 }SqList;
-
-int DeleteMin(SqList *L){
-    if(L->length == 0){
-        printf("EMPTY");
-        exit(1);
-    }
-    int min_value = L->data[0];
-    int pos = 0;
-    
-
-    for(int i=1;i<L->length;i++){
-        if(L->data[i]<min_value){
-             min_value = L->data[i];
-             pos = i;
-        }
-
-        
-    }
-
-    L->data[pos] = L->data[L->length-1];
-    L->length--;
-
-    return min_value;
+int Delete(SqList *L){
+if(L->length == 0) {
+    printf("empty\n");
+    exit(1);
 }
 
+int min_value = L->data[0];
+int pos = 0;
+for(int i =0;i<L->length;i++){
+    if(L->data[i]< min_value){
+        min_value = L->data[i];
+        pos = i;
+    }
+    }   
+L->data[pos] = L->data[L->length-1];
+L->length--;
+return min_value;
+}
 int main(){
     SqList L;
-    L.length =5;
-    L.data[0] = 10;
-    L.data[1] = 2;
-    L.data[2] = 56;
-    L.data[3] = 8;
-    L.data[4] = 99;
-    
-    printf("before print\n");
+    L.data[0] = 1;
+    L.data[1] = 4;
+    L.data[2] = 3;
+    L.data[3] = 2;
+    L.length = 4;
+    printf("before");
+    printf("\n");
     for(int i=0;i<L.length;i++){
-        printf("%d ",L.data[i]);
+    printf("%d ",L.data[i]);
     }
     printf("\n");
-    int val = DeleteMin(&L);
-    printf("%d\n",val);
-
-
+    int val = Delete(&L);
+    printf("delete value:%d\n",val);
     for(int i=0;i<L.length;i++){
-        printf("%d ",L.data[i]);
+    printf("%d ",L.data[i]);
     }
-    return 0;
+}
+    
 
-    }
+
+
+
+
 
 
     
--- Sept2/02ReverseList.c
+++ Sept2/02ReverseList.c
@@ -1,8 +1,20 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAXSIZE 100
+
+typedef struct {
+    int data[MAXSIZE];
+    int length;
+}SqList;
 void ReverseList(SqList *L){
-    int temp;
     for(int i=0;i<L->length/2;i++){
+        int temp = 0;
         temp = L->data[i];
-        L->data[i] = L->data[L->length-i-1];
-        L->data[L->length-i-1] = temp;
+        L->data[i] = L->data[L->length-1-i];
+        L->data[L->length-1-i] = temp;
     }
 }
+
+int main(){
+    
+
